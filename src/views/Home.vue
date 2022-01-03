88f0635598cab991dfb2c4aa2c5822e690e1f606
JoelGutierrezN <al221910321@gmail.com>
2022-01-03 16:48:00 -0600

    Avances al proyecto

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <px-assets-table :assets="assets"/>
+    </div>
+</template>
+
+<script>
+
+import PxAssetsTable from '@/components/PxAssetsTable';
+import api from '@/api';
+
+export default {
+    name: 'Home',
+    components: { PxAssetsTable },
+
+    data(){
+        return {
+            assets: []
+        }
+    },
+
+    created (){
+        api.getAssets()
+            .then( response => {
+                this.assets = response;
+            })
+    }
+}
+</script>
